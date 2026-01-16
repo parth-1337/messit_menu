34c5cedfcc3fdb4a2b16d3eb742a2e6acf9c1950
parth-1337 <parth.panchal2904@outlook.com>
2026-01-16 21:00:48 +0530

    first commit

---
+++ menu.py
@@ -0,0 +1,57 @@
+import google.generativeai as ai
+import json
+from PIL import Image
+
+
+API_KEY = "AIzaSyBIlu0zn3FGanmkBnq7KZsnw81M1mn44hQ"
+ai.configure(api_key=API_KEY)
+
+# 1.5 doesnt work only 2.5 flash
+# 3 flash-preview also working
+model = ai.GenerativeModel("gemini-2.5-flash")
+
+
+image_file = Image.open("menu.jpg")
+
+# image rotateer
+if image_file.height > image_file.width:
+    print("Rotated")
+    image_file = image_file.rotate(90, expand=True)
+
+# PROMPT
+prompt = """
+Look at this mess menu image. Extract the data into this exact JSON format:
+
+[
+  {
+    "date": "YYYY-MM-DD",
+    "menu": [
+      { "type": 1, "menu": "Food items here" },
+      { "type": 2, "menu": "Food items here" },
+      { "type": 3, "menu": "Food items here" },
+      { "type": 4, "menu": "Food items here" }
+    ]
+  }
+]
+
+Rules:
+1. Type 1 is Breakfast, 2 is Lunch, 3 is Snacks, 4 is Dinner.
+2. Convert dates to YYYY-MM-DD.
+3. If a slot is empty, just leave the string empty.
+4. Give me ONLY the JSON code. No extra text.
+5. IMPORTANT: Never use '\n' (newlines) inside the menu strings. If text is on multiple lines, join them with a comma and a space.
+"""
+
+print("image sent to api")
+response = model.generate_content([prompt, image_file])
+
+# added by claude removes that json added by gemini in output
+clean_json = response.text.replace("```json", "").replace("```", "").strip()
+
+
+# save
+
+with open("menu.json", "w") as my_file:
+    my_file.write(clean_json)
+
+print("Saved")
