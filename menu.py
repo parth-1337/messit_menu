2fed37ff7e4d95ce2bc5427b11a969bda5fa2411
Parth <parth.panchal1729@gmail.com>
2026-01-18 20:01:32 +0530

    used cv2-BGR2GRAY

--- menu.py
+++ menu.py
@@ -1,7 +1,8 @@
 import google.generativeai as ai
 import json
 from PIL import Image
-
+import cv2
+import numpy as np
 
 API_KEY = "AIzaSyBIlu0zn3FGanmkBnq7KZsnw81M1mn44hQ"
 ai.configure(api_key=API_KEY)
@@ -13,11 +14,21 @@ model = ai.GenerativeModel("gemini-2.5-flash")
 
 image_file = Image.open("menu.jpg")
 
+
 # image rotateer
+
 if image_file.height > image_file.width:
     print("Rotated")
     image_file = image_file.rotate(90, expand=True)
 
+# image cvt black and white
+grey_img = cv2.cvtColor(np.array(image_file), cv2.COLOR_BGR2GRAY)
+cv2.imwrite("grey_image.jpg", grey_img)
+
+# change array back to image format
+final_img = Image.fromarray(grey_img)
+
+'''
 # PROMPT
 prompt = """
 Look at this mess menu image. Extract the data into this exact JSON format:
@@ -43,7 +54,7 @@ Rules:
 """
 
 print("Image sent to api")
-response = model.generate_content([prompt, image_file])
+response = model.generate_content([prompt, final_img])
 
 # added by claude removes that json added by gemini in output
 clean_json = response.text.replace("```json", "").replace("```", "").strip()
@@ -55,4 +66,4 @@ with open("menu.json", "w") as my_file:
     my_file.write(clean_json)
 
 print("Saved")
-
+'''
